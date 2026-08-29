9c9ecf4cfb9c8e6019219bdc483264c320916b59
Lucas Janoca <Lucasjanoca9@gmail.com>
2026-08-29 03:31:52 -0300

    security: serve io installer files from Cloudflare

--- io/setup/INSTALAR-IO.cmd
+++ io/setup/INSTALAR-IO.cmd
@@ -6,7 +6,7 @@ title Instalador da io
 set "IO_PS1=%TEMP%\instalar-io.ps1"
 
 echo Baixando o instalador da io...
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing 'https://infotech-io.com.br/io/setup/instalar-io.ps1' -OutFile '%IO_PS1%'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing 'https://infotech-io.pages.dev/io/setup/instalar-io.ps1' -OutFile '%IO_PS1%'"
 if errorlevel 1 (
   echo.
   echo Nao foi possivel baixar o instalador da io.
